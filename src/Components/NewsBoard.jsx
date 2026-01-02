ea3b93496e7bbf9223568c64f91ed87189f83904
favour ololade <ololadefavourajayi@gmail.com>
2026-01-02 05:15:04 +0100

    env secured

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -1,22 +1,22 @@
 import { useEffect, useState } from "react";
 import NewsItem from "./NewsItem";
 
-const NewsBoard = () => {
+const NewsBoard = ({ category }) => {
 
     const [articles, setArticles] = useState([]);
 
     useEffect(() => {
-        let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_API_KEY}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
         fetch(url).then(response => response.json()).then(data => setArticles(data.articles));
 
-    }, [])
+    }, [category])
     return (
-        <divi>
+        <div>
             <h2 className="text-center">Latest <span className="badge  bg-danger">News</span></h2>
             {articles.map((news, index) => {
                 return <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url} />
             })}
-        </divi>
+        </div>
     )
 }
 
--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -1,12 +1,12 @@
-
+import image from '../assets/news.jpeg'
 
 const NewsItem = ({ title, description, src, url }) => {
   return (
-    <div className="card" style={{ maxWidth: "345px" }} >
-      <img src={src} className="card-img-top" alt="..." />
+    <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-4 px-2 py-2" style={{ maxWidth: "345px" }} >
+      <img src={src ? src : image} style={{ height: "200px", width: "330px" }} className="card-img-top" alt="..." />
       <div className="card-body">
-        <h5 className="card-title">{title} </h5>
-        <p className="card-text">{description} </p>
+        <h5 className="card-title">{title.slice(0, 50)} </h5>
+        <p className="card-text">{description ? description.slice(0, 90) : "It’s out with the old and in with the new  "} </p>
         <a href={url} className="btn btn-primary">Read More</a>
       </div>
     </div>
